ab8c1e6c130bb67b25f3da869deb5b7b3d10d596
JacksonJ01 <jacksonj@hartwick.edu>
2019-11-11 22:52:37 -0500

    Almost missed the deadline

---
+++ callsCars.py
@@ -0,0 +1,8 @@
+# Jackson J.
+# 11/11/19
+# This is a class creation of a car
+from classCar import Cars
+myCar = Cars("Mclaren", "GTR", "Yellow", 2019, "V8", 15, 2, 19800000, 8)
+myCar.car_des()
+myCar.change_color()
+myCar.meter()
--- classCar.py
+++ classCar.py
@@ -1,1 +1,46 @@
+# Jackson J.
+# 11/11/19
+# This is a class creation of a car
 
+
+class Cars:
+    def __init__(self, make, model, color, year, engine, mileage, doors, price, odometer):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.year = int(year)
+        self.engine = engine
+        self.mileage = int(mileage)
+        self.doors = int(doors)
+        self.price = int(price)
+        self.distance_traveled = int(odometer)
+
+    def car_des(self):
+        print(f"Wow that's interesting! I've never heard of a car like that.")
+        print(f"A {self.make}, model {self.model}, and you've got it in {self.color}?!")
+        print(f"You said it was made in {self.year}, right? That explains that {self.engine} engine and the {self.doors} door option.")
+        print(f"And with that engine, looks like your gas mileage is sitting at {self.mileage}.")
+        print(f"How much did you have to pay for that? Looks like it comes up to about ${self.price}.")
+
+    def change_color(self):
+        print("What color would you want to change your car to if you had the opportunity?")
+        nc = input(">>>")
+        self.color = nc
+        print(f"Okay, so you'd have a {self.color} {self.make}. How about you buy me a car like that.")
+        buy = input(">>>").title()
+        if buy == "Yes" or buy == "Y":
+            print("Really??? Wow, that's awesome...")
+            print("You may have to program a car for me though. Since...")
+
+        elif buy == "No" or buy == "N":
+            print("I figured you say that.")
+
+        else:
+            print("I'm going to assume that is a no")
+
+    def meter(self):
+        print("What is your odometer at?")
+        od = input(">>>")
+        self.odometer = od
+        print(f"You've traveled far! {self.odometer} miles")
+    # update odometer
