8ba0a3c4f11a80a27872e6294a16f8aeb330205d
JacksonJ01 <jacksonj@hartwick.edu>
2019-11-11 23:00:09 -0500

    Almost missed the deadline....

--- callsCars.py
+++ callsCars.py
@@ -2,7 +2,7 @@
 # 11/11/19
 # This is a class creation of a car
 from classCar import Cars
-myCar = Cars("Mclaren", "F1 GTR", "Yellow", 2019, "V8", 15, 2, 19800000, 8)
+myCar = Cars("Mclaren", "F1 GTR", "Yellow", 2019, "V8", 15, 2, 19800000, 0)
 myCar.car_des()
 myCar.change_color()
 myCar.meter()
